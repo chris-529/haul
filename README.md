6d47ffe7e64d4b22c4484fd77294a2531e0a20d7
chris-529 <chrisbuddy29@gmail.com>
2026-04-08 16:33:19 -0700

    DB schema work, had to adjust main.go to fit db

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Done: REST Api routes set up
 
 Done: image upload in demo ui -> formatted json receipt data (locally ran)
 
+Done: Database schema has been completed, with a join table for recipe ingredients since a single ingredient could belong to multiple different recipes. DB design is shown below in a diagram
+
 In progress: Currently working on setting up PostgreSQL db
 
 # Roadmap
@@ -21,3 +23,7 @@ In progress: Currently working on setting up PostgreSQL db
 - User accounts (bcrypt)
 - Groccery inventory + meal recommendations
 - Reduce costs by replacing Gemini API with OCR + lightweight LLM
+
+# DB Schema
+
+<img src="docs/db-diagram.png" width="400" />
--- cmd/main.go
+++ cmd/main.go
@@ -12,22 +12,25 @@ import (
 )
 
 type Item struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Price float64 `json:"price"`
-	Quantity float64 `json:"quantity"`
+    ID        string  `json:"id"`
+    ReceiptID string  `json:"receipt_id"`
+    Name      string  `json:"name"`
+    Price     float64 `json:"price"`
+    Quantity  float64 `json:"quantity"`
+	Unit      string  `json:"unit"`
 }
 
 type Receipt struct {
-	ID string `json:"id"`
-	Store string `json:"store"`
-	Status string `json:"status"`
-	Items []Item `json:"items"`
+    ID     string `json:"id"`
+    UserID string `json:"user_id"` 
+    Store  string `json:"store"`
+    Status string `json:"status"`
+    Items  []Item `json:"items"`
 }
 
 type Recipe struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
 	Ingredients []Item `json:"ingredients"`
 }
 
